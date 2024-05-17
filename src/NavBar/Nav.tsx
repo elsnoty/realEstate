5b02ffb3dbc21a254834d14e4b7f6e28eb0aea30
elsnoty <mostafashawky399@gmail.com>
2024-05-18 02:29:21 +0300

    hot fix#2

--- src/NavBar/Nav.tsx
+++ src/NavBar/Nav.tsx
@@ -20,7 +20,7 @@ const Nav = () => {
     }, []);
 
     return ( 
-        <nav className={`px-20 py-3 max-md:px-5 max-md:py-0  z-50 w-full duration-300 ${scrolling ? 'fixed bg-white' : 'fixed'}`}>
+        <nav className={`px-20 py-3 max-md:px-5 max-md:py-0  z-50 w-full duration-300 ${scrolling ? 'fixed bg-white' : 'relative'}`}>
             <NavBar />
         </nav>
      );
